dff3d556fdb936db711238c33d2ae7188ab438fb
RenanLMenezes <renanlmenezes2003@gmail.com>
2021-11-22 11:46:54 -0300

    aprimorando a API

--- nodejs-apis-restful/api/rotas/fornecedores/index.js
+++ nodejs-apis-restful/api/rotas/fornecedores/index.js
@@ -1,11 +1,57 @@
 const roteador = require('express').Router()
 const tabelaFornecedor = require('./tabelaFornecedor.js')
+const Fornecedor = require('./fornecedor.js')
 
-roteador.use('/', async (req, res) =>{
+roteador.get('/', async (req, res) => {
     const resultados = await tabelaFornecedor.listar()
     res.send(
         JSON.stringify(resultados)
     )
 })
+
+roteador.get("/:idFornecedor", async (req, res) => {
+    try {
+        const id = req.params.idFornecedor
+        const fornecedor = new Fornecedor({ id: id })
+        await fornecedor.carregar()
+        res.send(
+            JSON.stringify(fornecedor)
+        )
+    } catch (erro){
+        res.send(
+            JSON.stringify({
+                mensagem: erro.mensage
+            })
+        )
+    }
+
+})
+
+roteador.post('/', async (req, res) => {
+    const dadosRecebidos = req.body
+    const fornecedor = new Fornecedor(dadosRecebidos)
+    await fornecedor.criar()
+    res.send(
+        JSON.stringify(resultados)
+    )
+})
+
+roteador.put('/:idFornecedor', async (req, res) =>{
+    try{
+        const id = req.params.idFornecedor
+        const dadosRecebidos = req.body
+        const dados = Object.assign({}, dadosRecebidos, {id: id})
+        const fornecedor = new Fornecedor(dados)
+        await fornecedor.atualizar()
+        res.end()
+    } catch (erro) {
+        res.send(
+            JSON.stringify({
+                mensagem: erro.mensage
+            })
+        )
+    }
+    
+}) 
 
 module.exports = roteador
--- nodejs-apis-restful/api/rotas/fornecedores/tabelaFornecedor.js
+++ nodejs-apis-restful/api/rotas/fornecedores/tabelaFornecedor.js
@@ -1,7 +1,33 @@
+const { INSERT } = require('sequelize/types/lib/query-types')
+const Fornecedor = require('./fornecedor')
 const Modelo = require('./modeloTabelaFornecedor')
 
 module.exports = {
     listar(){
         return Modelo.findAll()
+    },
+    inserir(){
+        return Modelo.create(fornecedor)
+    },
+    async pegarPorId(id){
+        const encontrado = await Modelo.findOne({
+            where: {
+                id: id
+            }
+        })
+
+        if(!encontrado){
+            throw new Error ('Fornecedor não encontrado')
+        }
+
+        return encontrado
+    },
+    atualizar (id, dadosParaAtualizar){
+        return Modelo.update(
+            dadosParaAtualizar,
+            {
+                where: {id: id}
+            }
+        )
     }
 }
