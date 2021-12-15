0c3a625472e5296988de7d9209be23e940cffebb
RenanLMenezes <renanlmenezes2003@gmail.com>
2021-12-15 16:55:07 -0300

    data validation and delete/create method

--- nodejs-apis-restful/api/rotas/fornecedores/produtos/index.js
+++ nodejs-apis-restful/api/rotas/fornecedores/produtos/index.js
@@ -1,11 +1,39 @@
+const Produto = require('./Produto')
 const tabelaProduto = require('./tabelaProduto')
 const roteadorProdutos = require('express').Router({ mergeParams: true })
 
 roteadorProdutos.get('/', async( req, res) => {
-    const produtos = await tabelaProduto.listar(req.params.idFornecedor)
+    const produtos = await tabelaProduto.listar(req.fornecedor.idFornecedor)
     res.send(
         JSON.stringify(produtos)
     )
 })
+
+roteadorProdutos.post('/', async ( req, res, proximo) => {
+    try {
+        const idFornecedor = req.fornecedor.idFornecedor
+        const conteudo = req.body
+        const dados = Object.assign({}, corpo, {fornecedor: idFornecedor})
+        const produto = new Produto(dados)
+        await produto.criar()
+        res.status(201)
+        res.send(produto)
+    } catch (erro) {
+        proximo(erro)
+    }
+})
+
+roteadorProdutos.delete('/:id', async(req, res) => {
+    const dados = {
+        id: req.params.id,
+        fornecedor: req.fornecedor.idFornecedor
+    }
+
+    const produto = new Produto(dados)
+    await produto.apagar()
+    res.status(204)
+    res.end()
+})
+
 
 module.exports = roteadorProdutos
--- nodejs-apis-restful/api/rotas/fornecedores/produtos/tabelaProduto.js
+++ nodejs-apis-restful/api/rotas/fornecedores/produtos/tabelaProduto.js
@@ -1,11 +1,20 @@
 const modelo = require("./modeloTableProduto")
 
-
-
 module.exports = {
-    listar (idFornecedor){
+    listar(idFornecedor){
         return modelo.findAll({
             where: {
+                fornecedor: idFornecedor
+            }
+        })
+    },
+    inserir(dados){
+        return modelo.create(dados)
+    },
+    remover(idProduto, idFornecedor){
+        return modelo.destroy({
+            where: {
+                id: idProduto,
                 fornecedor: idFornecedor
             }
         })
